2fafb8cb820294b0faa05989a4c47bc6e1352ea6
Rafael B Machado <rafaeobm@hotmail.comm>
2019-04-23 00:10:29 -0300

    Creating the New Task Page

--- frontend/src/app/pages/new-list/new-list.component.ts
+++ frontend/src/app/pages/new-list/new-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { TaskService } from "src/app/task.service";
+import { Router } from "@angular/router";
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: "app-new-list",
@@ -7,14 +9,15 @@ import { TaskService } from "src/app/task.service";
   styleUrls: ["./new-list.component.scss"]
 })
 export class NewListComponent implements OnInit {
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private router: Router) {}
 
   ngOnInit() {}
 
   createList(title: string) {
-    this.taskService.createList(title).subscribe((response: any) => {
-      console.log(response);
+    this.taskService.createList(title).subscribe((list: List) => {
+      console.log(list);
       // Now we navigate to /lists/response._id
+      this.router.navigate(["/lists", list._id]);
     });
   }
 }
