b68c460a0b8e4ca68679c9820bf02f2454cbe5ae
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2019-04-23 00:10:29 -0300

    Creating the New Task Page

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -7,16 +7,21 @@ import { WebRequestService } from "./web-request.service";
 export class TaskService {
   constructor(private webReqService: WebRequestService) {}
 
+  getLists() {
+    return this.webReqService.get("lists");
+  }
+
   createList(title: string) {
     // We want to send a web request to create a list
     return this.webReqService.post("lists", { title });
   }
 
-  getLists() {
-    return this.webReqService.get("lists");
-  }
-
   getTasks(listId: string) {
     return this.webReqService.get(`lists/${listId}/tasks`);
+  }
+
+  createTask(title: string, listId: string) {
+    // We want to send a web request to create a task
+    return this.webReqService.post(`lists/${listId}/tasks`, { title });
   }
 }
