ccc72d24176247210a926ae5653a7777289226c1
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2019-04-24 23:43:34 -0300

    Signup page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NewListComponent } from "./pages/new-list/new-list.component";
 import { NewTaskComponent } from "./pages/new-task/new-task.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { WebReqInterceptor } from "./web-req.interceptor";
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { WebReqInterceptor } from "./web-req.interceptor";
     TaskViewComponent,
     NewListComponent,
     NewTaskComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -29,6 +29,21 @@ export class AuthService {
     );
   }
 
+  signup(email: string, password: string) {
+    return this.webService.signup(email, password).pipe(
+      shareReplay(),
+      tap((res: HttpResponse<any>) => {
+        // the auth tokens will be in the header of this response
+        this.setSession(
+          res.body._id,
+          res.headers.get("x-access-token"),
+          res.headers.get("x-refresh-token")
+        );
+        console.log("Successfully signed up and now logged in!");
+      })
+    );
+  }
+
   logout() {
     this.removeSession();
     this.router.navigateByUrl("/login");
